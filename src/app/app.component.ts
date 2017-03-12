b395830bf2bcf0db027b1a0c7f5dd8e912c51f66
Rahil Shaikh <Rahil Shaikh>
2017-03-12 17:34:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PushNotificationsService } from 'angular2-notifications'; //import the service
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Web push Notifications!';
+
+  constructor(private _pushNotifications: PushNotificationsService){
+    _pushNotifications.requestPermission(); // request for permission as soon as component loads
+  }
+
+  notify(){ //our function to be called on click
+    let options = { //set options
+      body: "The truth is, I'am Iron Man!",
+      icon: "assets/images/ironman.png" //adding an icon
+    }
+    let notify = this._pushNotifications.create('Iron Man', options).subscribe( //creates a notification
+        res => console.log(res),
+        err => console.log(err)
+    );
+  }
 }
